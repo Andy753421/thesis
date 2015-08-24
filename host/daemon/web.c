9fb8b92424bba746f21d9f9fa5502020d731df23
Andy Spencer <andy753421@gmail.com>
2015-08-24 06:52:40 +0000

    Work on WebSocket receiving

--- host/daemon/web.c
+++ host/daemon/web.c
@@ -60,27 +60,39 @@ typedef enum {
 	MD_FIELD,
 	MD_VALUE,
 	MD_BODY,
+
+	MD_OPCODE,
+	MD_LENGTH,
+	MD_EXTEND,
+	MD_MASKED,
+	MD_DATA,
 } md_t;
 
 struct web_t {
-	// Web socket stuff
+	// Common stuff
 	int   cache;
 	int   sock;
 	int   mode;
 	int   idx;
 
-	// Header attributes
-	int   connect;
-	int   upgrade;
-	int   accept;
-	char  key[28];
+	// Http Request
+	char  req_method[MAX_METHOD+1];
+	char  req_path[MAX_PATH+1];
+	char  req_version[MAX_VERSION+1];
 
-	// Parser strings
-	char  method[MAX_METHOD+1];
-	char  path[MAX_PATH+1];
-	char  version[MAX_VERSION+1];
-	char  field[MAX_FIELD+1];
-	char  value[MAX_VALUE+1];
+	// Http Headers
+	int   hdr_connect;
+	int   hdr_upgrade;
+	int   hdr_accept;
+	char  hdr_key[28];
+	char  hdr_field[MAX_FIELD+1];
+	char  hdr_value[MAX_VALUE+1];
+
+	// WebSocket attributes
+	uint8_t  ws_opcode;
+	uint8_t  ws_masked;
+	uint8_t  ws_mask[4];
+	uint64_t ws_length;
 };
 
 /* Local data */
@@ -122,10 +134,10 @@ static void web_head(web_t *web, const char *field, const char *value)
 
 	debug("    Web Head: %-20s -> [%s]", field, value);
 	if (!strcasecmp(field, "Upgrade")) {
-		web->upgrade = !!strcasestr(value, "websocket");
+		web->hdr_upgrade = !!strcasestr(value, "websocket");
 	}
 	else if (!strcasecmp(field, "Connection")) {
-		web->connect = !!strcasestr(value, "upgrade");
+		web->hdr_connect = !!strcasestr(value, "upgrade");
 	}
 	else if (!strcasecmp(field, "Sec-WebSocket-Key")) {
 #ifdef USE_GNUTLS
@@ -142,15 +154,15 @@ static void web_head(web_t *web, const char *field, const char *value)
 		SHA1_Update(&sha1, extra, strlen(extra));
 		SHA1_Final((unsigned char*)hash, &sha1);
 #endif
-		web->accept = base64(hash, sizeof(hash),
-			web->key, sizeof(web->key));
+		web->hdr_accept = base64(hash, sizeof(hash),
+			web->hdr_key, sizeof(web->hdr_key));
 	}
 }
 
 static int web_body(web_t *web)
 {
-	debug("  Web Body: path=%s", web->path);
-	if (!strcmp(web->path, "/")) {
+	debug("  Web Body: path=%s", web->req_path);
+	if (!strcmp(web->req_path, "/")) {
 		web_printf(web->sock,
 			"HTTP/1.1 200 OK\r\n"
 			"Content-Type: text/html; charset=UTF-8\r\n"
@@ -162,21 +174,21 @@ static int web_body(web_t *web)
 			"</html>\r\n"
 		);
 	}
-	else if (!strcmp(web->path, "/socket")) {
+	else if (!strcmp(web->req_path, "/socket")) {
 		debug("  Web Sock: connect=%d upgrade=%d accept=%d\n"
 		      "            key=%s",
-		      web->connect, web->upgrade, web->accept, web->key);
+		      web->hdr_connect, web->hdr_upgrade, web->hdr_accept, web->hdr_key);
 
-		if (web->connect && web->upgrade && web->accept) {
+		if (web->hdr_connect && web->hdr_upgrade && web->hdr_accept) {
 			web_printf(web->sock,
 				"HTTP/1.1 101 WebSocket Protocol Handshake\r\n"
 				"Connection: Upgrade\r\n"
 				"Upgrade: WebSocket\r\n"
 				"Sec-WebSocket-Accept: %.*s\r\n"
 				"\r\n",
-				web->accept, web->key
+				web->hdr_accept, web->hdr_key
 			);
-			return 0;
+			return MD_OPCODE;
 		}
 		else {
 			web_printf(web->sock,
@@ -216,12 +228,17 @@ web_t *web_add(int sock)
 {
 	web_t *web = alloc_get_ptr(&alloc, peers);
 	if (web) {
-		web->sock    = sock;
-		web->mode    = 0;
-		web->idx     = 0;
-		web->connect = 0;
-		web->upgrade = 0;
-		web->accept  = 0;
+		web->sock        = sock;
+		web->mode        = 0;
+		web->idx         = 0;
+
+		web->hdr_connect = 0;
+		web->hdr_upgrade = 0;
+		web->hdr_accept  = 0;
+
+		web->ws_opcode   = 0;
+		web->ws_masked   = 0;
+		web->ws_length   = 0;
 	}
 	return web;
 }
@@ -239,6 +256,9 @@ int web_send(web_t *web, char *buf, int len)
 	uint8_t  *flag  = (uint8_t *)&head[1];
 	uint16_t *ext16 = (uint16_t*)&head[2];
 	uint64_t *ext64 = (uint64_t*)&head[2];
+
+	if (web->mode < MD_OPCODE)
+		return 0;
 
 	if (len < 126) {
 		bytes  = 2;
@@ -266,57 +286,134 @@ int web_parse(web_t *web, char **buf, int *len)
 {
 	int status = 0;
 	for (int i = 0; i < *len; i++) {
-		int      ch = (*buf)[i];
+		uint8_t ch = (*buf)[i];
 
-		if (ch == '\r')
-			continue;
-		if (ch == ' ' && web->idx == 0)
-			continue;
+		if (web->mode < MD_BODY) {
+			if (ch == '\r')
+				continue;
+			if (ch == ' ' && web->idx == 0)
+				continue;
+		}
 
 		switch (web->mode) {
+			/* HTTP Parsing */
 			case MD_METHOD:
 				if (ch == ' ')
-					web_mode(web, MD_PATH, web->method);
+					web_mode(web, MD_PATH, web->req_method);
 				else if (web->idx < MAX_METHOD)
-					web->method[web->idx++] = ch;
+					web->req_method[web->idx++] = ch;
 				break;
 
 			case MD_PATH:
 				if (ch == ' ')
-					web_mode(web, MD_VERSION, web->path);
+					web_mode(web, MD_VERSION, web->req_path);
 				else if (web->idx < MAX_PATH)
-					web->path[web->idx++] = ch;
+					web->req_path[web->idx++] = ch;
 				break;
 
 			case MD_VERSION:
 				if (ch == '\n') {
-					web_mode(web, MD_FIELD, web->version);
-					web_open(web, web->method, web->path, web->version);
+					web_mode(web, MD_FIELD, web->req_version);
+					web_open(web, web->req_method,
+					              web->req_path,
+					              web->req_version);
 				}
 				else if (web->idx < MAX_VERSION)
-					web->version[web->idx++] = ch;
+					web->req_version[web->idx++] = ch;
 				break;
 
 			case MD_FIELD:
 				if (ch == '\n') {
 					if ((status = web_body(web)) >= 0)
-						web_mode(web, MD_BODY, web->field);
+						web_mode(web, status, web->hdr_field);
 				} else if (ch == ':')
-					web_mode(web, MD_VALUE, web->field);
+					web_mode(web, MD_VALUE, web->hdr_field);
 				else if (web->idx < MAX_FIELD)
-					web->field[web->idx++] = ch;
+					web->hdr_field[web->idx++] = ch;
 				break;
 
 			case MD_VALUE:
 				if (ch == '\n') {
-					web_mode(web, MD_FIELD, web->value);
-					web_head(web, web->field, web->value);
+					web_mode(web, MD_FIELD, web->hdr_value);
+					web_head(web, web->hdr_field, web->hdr_value);
 				} else if (web->idx < MAX_VALUE)
-					web->value[web->idx++] = ch;
+					web->hdr_value[web->idx++] = ch;
 				break;
 
 			case MD_BODY:
 				debug("  Web Body: ...");
+				break;
+
+			/* Web Socket Parsing */
+			case MD_OPCODE:
+				web->ws_opcode = ch;
+				web->mode      = MD_LENGTH;
+				break;
+
+			case MD_LENGTH:
+				web->ws_length = (ch&0x7F);
+				web->ws_masked = (ch&0x80)!=0;
+
+				debug("web_parse: length - ch=%02hhx, masked=%hhd, length=%hhd",
+						ch, web->ws_masked, web->ws_length);
+
+				if (web->ws_length == 126) {
+					web->ws_length = 0;
+					web->idx       = 2;
+					web->mode      = MD_EXTEND;
+				}
+				else if (web->ws_length == 127) {
+					web->ws_length = 0;
+					web->idx       = 8;
+					web->mode      = MD_EXTEND;
+				}
+				else if (web->ws_masked) {
+					web->mode      = MD_MASKED;
+				}
+				else {
+					web->mode      = MD_DATA;
+				}
+				break;
+
+			case MD_EXTEND:
+				debug("web_parse: extend");
+				web->idx       -= 1;
+				web->ws_length |= ((uint64_t)ch) << (web->idx*8);
+				if (web->idx == 0) {
+					if (web->ws_masked)
+						web->mode = MD_MASKED;
+					else
+						web->mode = MD_DATA;
+				}
+				break;
+
+			case MD_MASKED:
+				debug("web_parse: masked - %d=%02x", web->idx, ch);
+				web->ws_mask[web->idx++] = ch;
+				if (web->idx >= 4) {
+					web->idx  = 0;
+					web->mode = MD_DATA;
+				}
+				break;
+
+			case MD_DATA:
+				debug("web_parse: data   - "
+						"op=%d:%02x "
+						"len=%d:%d "
+						"mask=%02hhx%02hhx%02hhx%02hhx "
+						"ch=%02hhx:'%c'",
+						web->ws_opcode == 0x80,
+						web->ws_opcode &  0x0F,
+						web->ws_masked,
+						web->ws_length,
+						web->ws_mask[0], web->ws_mask[1],
+						web->ws_mask[2], web->ws_mask[3],
+						ch,
+						ch ^ web->ws_mask[web->idx++%4]);
+				if (web->idx == web->ws_length) {
+					web->idx  = 0;
+					web->mode = MD_OPCODE;
+				}
 				break;
 		}
 	}
